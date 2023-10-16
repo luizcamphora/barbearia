b58acec05a57308f352b6ccfbb70a31ad3b45ca2
manager-project <luiz.camphora@outlook.com>
2023-10-16 02:24:32 -0300

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navegacao">
+        <nav class="navbar bg-body-tertiary" id="navegacao-logo">
+          <p id="texto-logo">Barbearia Prossifional</p>
+          <div class="container" id="logo">
+            <a class="navbar-brand" href="#" id="logo-txt">Barbearia da Vila </a>
+            <img src="/img/logo.jpg" alt="Minha Imagem" id="logo-img" />
+          </div>
+          <p id="texto-logo">desde 1996</p>
+        </nav>
+        <nav class="navbar bg-body-tertiary" >
+          <div class="container-fluid" id="navegacao-link">
+            <a class="navbar-brand" href="#" id="link-lista">Página Incial</a>
+            <a class="navbar-brand" href="#" id="link-lista">Produtos</a>
+            <a class="navbar-brand" href="#" id="link-lista">Agenda</a>
+            <a class="navbar-brand" href="#" id="link-lista">Quem Somos</a>
+          </div>
+        </nav>
+      </div>
+      <div id="principal">
+        <div id="banner">
+        </div>
+      </div>
     </div>
   );
 }

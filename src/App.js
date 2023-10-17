f494a182ac233f2db76403f30e508b5590f32527
manager-project <luiz.camphora@outlook.com>
2023-10-17 01:22:04 -0300

    version 1.1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <div className="navegacao">
         <nav class="navbar bg-body-tertiary" id="navegacao-logo">
-          <p id="texto-logo">Barbearia Prossifional</p>
+          <p id="texto-logo">Barbearia Profissional</p>
           <div class="container" id="logo">
             <a class="navbar-brand" href="#" id="logo-txt">Barbearia da Vila </a>
             <img src="/img/logo.jpg" alt="Minha Imagem" id="logo-img" />
           </div>
-          <p id="texto-logo">desde 1996</p>
+          <p id="texto-logo-desde">desde 1996</p>
         </nav>
         <nav class="navbar bg-body-tertiary" >
           <div class="container-fluid" id="navegacao-link">

b2f0489c46dbcb643d14ad8cacf13f6f164371f0
manager-project <luiz.camphora@outlook.com>
2023-10-18 02:31:28 -0300

    version 1.4

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Button } from 'bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,7 +7,7 @@ function App() {
     <div className="App">
       <div className="navegacao">
         <nav id="navegacao-logo">
-          <p id="texto-logo">Barbearia Profissional</p>
+        <p id="texto-logo">Barbearia Profissional</p>    
           <div class="container" id="logo">
             <a class="navbar-brand" href="#" id="logo-txt">Barbearia da Vila </a>
             <img src="/img/logo.jpg" alt="Minha Imagem" id="logo-img" />
@@ -16,7 +17,7 @@ function App() {
         <nav id="link-nav">
           <div class="container-fluid" id="navegacao-link">
             <a class="navbar-brand" href="#" id="link-lista">Página Incial</a>
-            <a class="navbar-brand" href="#" id="link-lista">Produtos</a>
+            <a class="navbar-brand" href="#" id="link-lista">Serviços</a>
             <a class="navbar-brand" href="#" id="link-lista">Agenda</a>
             <a class="navbar-brand" href="#" id="link-lista">Quem Somos</a>
           </div>
@@ -24,10 +25,24 @@ function App() {
       </div>
       <div id="principal">
         <div id="banner">
-          <img src="/img/banner.jpg" id="imagem-banner"></img>
+          <img src="/img/barnner-first.jpg" id="imagem-banner"></img>
         </div>
           <div id="produtos">
-            <p id="produtos-titulo">Produtos</p>
+            <p id="produtos-titulo">Serviços</p>
+            <div id="produtos-cards">
+              <div id="cards"> 
+                <div id="cards-img">
+                  <img src="/img/aparar.jpg" ></img>
+                </div>
+                <p>Corte Tradicional</p>
+                <p>R$ 50,00</p>
+                <button>Agendar</button>
+              </div>
+              <div id="cards"> 
+              </div>
+              <div id="cards"> 
+              </div>
+            </div>
           </div>
       </div>
     </div>

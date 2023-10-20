66f69a8155f3bb74c2b52940d011459393cc6843
manager-project <luiz.camphora@outlook.com>
2023-10-20 01:07:12 -0300

    version 1.5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { Button } from 'bootstrap';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 function App() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    });
+  }, []);
   return (
     <div className="App">
       <div className="navegacao">
-        <nav id="navegacao-logo">
+        <nav id="navegacao-logo" className={isScrolled ? 'navegacao-escondida' : ''}>
           <p id="texto-logo">Barbearia Profissional</p>
           <div class="container" id="logo">
             <a class="navbar-brand" href="#" id="logo-txt">Barbearia da Vila </a>
@@ -14,19 +25,19 @@ function App() {
           </div>
           <p id="texto-logo-desde">desde 1996</p>
         </nav>
-        <nav id="link-nav">
+        <nav id="link-nav" className={isScrolled ? 'nav-fixa' : ''}>
           <div class="container-fluid" id="navegacao-link">
             <a class="navbar-brand" href="#" id="link-lista">Página Inicial</a>
             <a class="navbar-brand" href="#" id="link-lista">Serviços</a>
             <a class="navbar-brand" href="#" id="link-lista">Agenda</a>
             <a class="navbar-brand" href="#" id="link-lista">Quem Somos</a>
           </div>
+          <div id="banner">
+            <img src="/img/barnner-first.jpg" id="imagem-banner"></img>
+          </div>
         </nav>
       </div>
       <div id="principal">
-        <div id="banner">
-          <img src="/img/barnner-first.jpg" id="imagem-banner"></img>
-        </div>
         <div id="produtos">
           <p id="produtos-titulo">Serviços</p>
           <div id="produtos-cards">
@@ -41,7 +52,7 @@ function App() {
               </div>
             </div>
             <div id="cards">
-            <div id="cards-img">
+              <div id="cards-img">
                 <img src="/img/barba.jpg" ></img>
               </div>
               <p id="titulo-card">Barba</p>
@@ -51,7 +62,7 @@ function App() {
               </div>
             </div>
             <div id="cards">
-            <div id="cards-img">
+              <div id="cards-img">
                 <img src="/img/corte+barba.jpg" ></img>
               </div>
               <p id="titulo-card">Corte + Barba</p>
@@ -63,10 +74,10 @@ function App() {
           </div>
         </div>
         <div id="agenda">
-        <p id="produtos-titulo"></p>
+          <p id="produtos-titulo"></p>
         </div>
         <div id="quem-somos">
-        <p id="produtos-titulo"></p>
+          <p id="produtos-titulo"></p>
         </div>
       </div>
     </div>
